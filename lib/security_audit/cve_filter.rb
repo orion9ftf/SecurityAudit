be988a4d3b953db3b568120894b62259bc91e991
m4pher <eva.matus@hotmail.com>
2026-05-23 17:59:20 -0400

    temporal change -- cve filter

--- lib/security_audit/cve_filter.rb
+++ lib/security_audit/cve_filter.rb
@@ -1,14 +1,27 @@
-module SecurityAudit
-    class CveFilter
-        def self.filter(dependency, cves)
-            cves.select do |cve|
-                affected_versions = cve[:affected_versions] || []
+# module SecurityAudit
+#     class CveFilter
+#         def self.filter(dependency, cves)
+#             cves.select do |cve|
+#                 affected_versions = cve[:affected_versions] || []
 
-                VersionMatcher.vulnerable?(
-                    dependency[:version],
-                    affected_versions
-                )
-            end
-        end
+#                 VersionMatcher.vulnerable?(
+#                     dependency[:version],
+#                     affected_versions
+#                 )
+#             end
+#         end
+#     end
+# end
+
+
+
+module SecurityAudit
+  class CveFilter
+    def self.filter(dependency, cves)
+      return [] unless cves["vulnerabilities"]
+
+      cves["vulnerabilities"]
     end
+  end
 end
+
