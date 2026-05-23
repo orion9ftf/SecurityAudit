9fedc74359c22b96634ba96c16216dcf08c3f006
m4pher <eva.matus@hotmail.com>
2026-05-23 18:38:42 -0400

    cv client - response

--- lib/security_audit/cve_client.rb
+++ lib/security_audit/cve_client.rb
@@ -2,14 +2,15 @@ require "net/http"
 require "json"
 
 module SecurityAudit
-    class CveClient
-        BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+  class CveClient
+    BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
 
-        def self.search(package_name)
-            url = URI("#{BASE_URL}?keywordSearch=#{package_name}")
+    def self.search(package_name)
+      url = URI("#{BASE_URL}?keywordSearch=#{package_name}")
 
-            response = Net::HTTP.get(url)
-            JSON.parse(response)
-        end
+      response = Net::HTTP.get_response(url)
+      return {} unless response.code == "200"
+      JSON.parse(response.body)
     end
+  end
 end
