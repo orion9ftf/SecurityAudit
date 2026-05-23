7d04b04390983a400e3eb17d7a392ebc74a24cfc
m4pher <eva.matus@hotmail.com>
2026-05-23 17:56:51 -0400

    security_audit/cve_filter

--- lib/security_audit.rb
+++ lib/security_audit.rb
@@ -1,6 +1,7 @@
 require "security_audit/version"
 require "security_audit/engine"
 require "security_audit/environment_detector"
+require "security_audit/cve_filter"
 require "security_audit/gemfile_scanner"
 require "security_audit/dependency_normalizer"
 require "security_audit/cve_client"
