18d8a413b46a4ce75057a4732c708aa6d1d97497
m4pher <eva.matus@hotmail.com>
2026-05-24 22:08:50 -0400

    Fix Gem requirement format in CPE version matching

--- lib/security_audit/cve_filter.rb
+++ lib/security_audit/cve_filter.rb
@@ -4,31 +4,24 @@ module SecurityAudit
       return [] unless cves["vulnerabilities"]
 
       cves["vulnerabilities"].select do |vuln|
-
         configs = vuln.dig("cve", "configurations") || []
-
         configs.any? do |config|
           nodes = config["nodes"] || []
-
           nodes.any? do |node|
             matches = node["cpeMatch"] || []
-
             matches.any? do |match|
-
               criteria = match["criteria"]
 
               next false unless criteria&.include?(
                 ":#{dependency[:package]}:"
               )
+              requirements = []
+              requirements << ">= #{match['versionStartIncluding']}" if match["versionStartIncluding"]
+              requirements << "< #{match['versionEndExcluding']}" if match["versionEndExcluding"]
 
               VersionMatcher.vulnerable?(
                 dependency[:version],
-                [
-                  {
-                    start: match["versionStartIncluding"],
-                    end: match["versionEndExcluding"]
-                  }
-                ]
+                requirements
               )
             end
           end
